83540d419c3439036c5bf21040faf4487bda327c
rohan <rohanbaisantry7@gmail.com>
2021-04-17 04:43:33 +0530

    fixes

--- constants/index.js
+++ constants/index.js
@@ -5,5 +5,4 @@ let envs = {
     development,
     production,
 };
-console.log(process.env.NODE_ENV);
-module.exports = envs[process.env.APP_ENV || "development"];
+module.exports = envs[process.env.NODE_ENV];
