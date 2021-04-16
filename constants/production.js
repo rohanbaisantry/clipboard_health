6a534aeac0156184c3358e4933d992a57012e4f2
rohan <rohanbaisantry7@gmail.com>
2021-04-17 03:43:57 +0530

    changes for prod

--- constants/production.js
+++ constants/production.js
@@ -1,5 +1,4 @@
 module.exports = {
-    mongoURI:
-        "mongodb+srv://rohan:Rohan_Atlas97@cluster0.1nsq1.mongodb.net/clipboard_health?retryWrites=true&w=majority",
-    apiURL: "http://localhost:3000/api",
+    mongoURI: process.env.MONGODB_URI,
+    apiURL: "",
 };
