e4f8f42c65ef69065994112080719f8d45c07ab5
rohan <rohanbaisantry7@gmail.com>
2021-04-17 15:13:33 +0530

    remove mongo password

--- constants/development.js
+++ constants/development.js
@@ -1,5 +1,4 @@
 module.exports = {
-    mongoURI:
-        "mongodb+srv://rohan:Rohan_Atlas97@cluster0.1nsq1.mongodb.net/clipboard_health?retryWrites=true&w=majority",
+    mongoURI: "",
     apiURL: "http://localhost:3000/api",
 };
--- constants/production.js
+++ constants/production.js
@@ -1,5 +1,4 @@
 module.exports = {
-    mongoURI:
-        "mongodb+srv://rohan:Rohan_Atlas97@cluster0.1nsq1.mongodb.net/clipboard_health?retryWrites=true&w=majority",
+    mongoURI: process.env.MONGODB_URI,
     apiURL: "https://clipboard-health-rohanbaisantry.vercel.app/api",
 };
